20a2f6a106f70ea4ebdf33ee42f07f54578565f1
Sadjow Leão <sadjow@gmail.com>
2014-11-04 11:30:09 -0200

    add a verbose option

--- test/makesure.js
+++ test/makesure.js
@@ -4,6 +4,11 @@ var should = require('should');
 var validator = require('validator');
 
 describe("Makesure API", function(){
+
+  it("has a verbose option", function(){
+    makesure.verbose.should.be.false;
+  });
+
   describe("makesure()", function(){
     it("returns a function", function(){
       (typeof makesure(function(){})).should.eql('function');
@@ -31,7 +36,7 @@ describe("Makesure API", function(){
 
 
   describe("nested validation", function(){
-    it("returns the error on first callback attribute and the sanitized object on second attribute", function(done){
+    it("HERE returns the error on first callback attribute and the sanitized object on second attribute", function(done){
       var empty = function(v){
         return v.length == 0
       }
