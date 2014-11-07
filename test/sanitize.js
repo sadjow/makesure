bc35e4e03ae647c0ae1aa1a0bcc7cac155f7a188
Sadjow Leão <sadjow@gmail.com>
2014-11-07 15:45:42 -0200

    Init sanitize()

---
+++ test/sanitize.js
@@ -0,0 +1,32 @@
+var makesure = require('..');
+var should = require('should');
+var validator = require('validator');
+var removeDiacritics = require('diacritics').remove;
+
+describe("makesure's sanitize API", function(){
+  describe("sanitize()", function(){
+    var sms = {
+      phone: "(99) 999-999-999",
+      message: "Olá, essa é sua mensagem." // portuguese mensagem test
+    }
+
+    it("sanitizes the accents and remove formatting", function(done){
+      var validateSMS = makesure(function(){
+        this.sanitize('phone', function(value) {
+          return value.replace( /\D+/g, '');
+        });
+
+        this.sanitize('message', function(value) {
+          return removeDiacritics(value); // remove accents
+        });
+      });
+
+      validateSMS(sms, function(err, newSMS){
+        should.not.exist(err);
+        newSMS.phone.should.eql('99999999999')
+        newSMS.message.should.eql('Ola, essa e sua mensagem.')
+        done();
+      });
+    });
+  });
+});
