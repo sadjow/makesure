3443bc6e8eba1d5cf37c959aaec4eca4ddc262bc
Sadjow Leão <sadjow@gmail.com>
2014-11-07 16:32:09 -0200

    update the sanitized object on a sub validation

--- test/sanitize.js
+++ test/sanitize.js
@@ -29,4 +29,37 @@ describe("makesure's sanitize API", function(){
       });
     });
   });
+
+
+  describe("sanitize() with sub object", function(){
+    var sms = {
+      phone: "(99) 999-999-999",
+      message: "Olá, essa é sua mensagem." // portuguese mensagem test
+    }
+
+    var body = { sms: sms };
+
+    it("sanitizes the accents and remove formatting", function(done){
+      var validateSMS = makesure(function(){
+        this.sanitize('phone', function(value) {
+          return value.replace( /\D+/g, '');
+        });
+
+        this.sanitize('message', function(value) {
+          return removeDiacritics(value); // remove accents
+        });
+      });
+
+      var validateAction = makesure(function(){
+        this.attr('sms').with(validateSMS);
+      });
+
+      validateAction(body, function(err, newSMS){
+        should.not.exist(err);
+        newSMS.sms.phone.should.eql('99999999999')
+        newSMS.sms.message.should.eql('Ola, essa e sua mensagem.')
+        done();
+      });
+    });
+  });
 });
